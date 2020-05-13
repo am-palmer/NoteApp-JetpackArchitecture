ca40239908f566b74b8c0769ec49036a7e06e902
unknown <am.palmer@outlook.com>
2020-05-13 17:28:25 +1200

    cleaned up mainactivity

--- app/src/main/java/amichaelpalmer/kotlin/noteappjetpack/MainActivity.kt
+++ app/src/main/java/amichaelpalmer/kotlin/noteappjetpack/MainActivity.kt
@@ -1,27 +1,14 @@
 package amichaelpalmer.kotlin.noteappjetpack
 
-import amichaelpalmer.kotlin.noteappjetpack.viewmodel.NoteViewModel
-import amichaelpalmer.kotlin.noteappjetpack.viewmodel.NoteViewModelFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.jetpackarchitecturedemo.R
 
-// todo: finish writing fragments, then implement navigation graph with safeargs
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-        // todo: launch mainfragment and pass viewmodel
-
-        //var navController = findNavController(R.id.activity_nav_host_fragment)
-
 
     }
 }
